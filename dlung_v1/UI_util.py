ffbb8ba78fdcf2a487cbf64ea5b377a32542107e
jiazewang <1155114536@link.cuhk.edu.hk>
2021-01-31 21:18:25 +0800

    Update UI_util.py

--- dlung_v1/UI_util.py
+++ dlung_v1/UI_util.py
@@ -8,8 +8,44 @@ import cv2
 
 from scipy.ndimage.interpolation import zoom
 from torch.autograd import Variable
-sys.path.append('../lung_nodule_detector')
-from training.layers import nms
+
+def iou(box0, box1):
+
+    r0 = box0[3] / 2
+    s0 = box0[:3] - r0
+    e0 = box0[:3] + r0
+
+    r1 = box1[3] / 2
+    s1 = box1[:3] - r1
+    e1 = box1[:3] + r1
+
+    overlap = []
+    for i in range(len(s0)):
+        overlap.append(max(0, min(e0[i], e1[i]) - max(s0[i], s1[i])))
+
+    intersection = overlap[0] * overlap[1] * overlap[2]
+    union = box0[3] * box0[3] * box0[3] + box1[3] * box1[3] * box1[3] - intersection
+    return intersection / union
+
+def nms(output, nms_th):
+    if len(output) == 0:
+        return output
+
+    output = output[np.argsort(-output[:, 0])]
+    bboxes = [output[0]]
+
+    for i in np.arange(1, len(output)):
+        bbox = output[i]
+        flag = 1
+        for j in range(len(bboxes)):
+            if iou(bbox[1:5], bboxes[j][1:5]) >= nms_th:
+                flag = -1
+                break
+        if flag == 1:
+            bboxes.append(bbox)
+
+    bboxes = np.asarray(bboxes, np.float32)
+    return bboxes
 
 def load_itk_image(filename):
     with open(filename) as f:
@@ -401,6 +437,6 @@ def crop_nodule_arr_2ch(target, img_arr, crop_size = 48):
 def predict_attribute(attribute_net, crop_img):
     attribute_net.eval()
     with torch.no_grad():
-        crop_img = Variable(crop_img.cuda(async=True))
+        crop_img = Variable(crop_img.cuda(non_blocking=True))
         output = attribute_net(crop_img)
     return output
