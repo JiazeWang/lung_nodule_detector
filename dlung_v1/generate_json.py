e2e44f0ac0981176e12585f5b19bb6c9d3e69f2a
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 22:01:07 +0800

    Update generate_json.py

--- dlung_v1/generate_json.py
+++ dlung_v1/generate_json.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     detect_th  = 0.3
     for i in range(len(pbb)):
         nms_pbb = nms(pbb[i], nms_th)
-        world_pbb = convert_worldcoord(i, nms_pbb, filename_dict[i])
+        world_pbb = convert_worldcoord(i, nms_pbb, config["npy_dir"]+filename_dict[i])
         s_id = filename_dict[i]
         for candidate in world_pbb:
             csv_submit.append([s_id, candidate[1], candidate[2], candidate[3], candidate[4], candidate[0]])
