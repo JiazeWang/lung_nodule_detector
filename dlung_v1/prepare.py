42a5e43944a53b148292881d1f2b111d3fae678b
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 20:31:44 +0800

    Update prepare.py

--- dlung_v1/prepare.py
+++ dlung_v1/prepare.py
@@ -262,8 +262,18 @@ def main():
     pool.close()
     pool.join()
     print(record_series)
-    with open("record_series.txt",'w') as f:
+    with open("record_folder_series.txt",'w') as f:
         f.write('\n'.join(record_series))
+
+    record_name = []
+    for line in record_series:
+        line = line.rstrip()
+        line = line.split('    ')
+        for i in range(1, len(line)):
+            record_name.append(line[i])
+    with open("record_series_list.txt",'w') as f:
+        f.write('\n'.join(record_series))
+
 
     """
     for line in lines:
