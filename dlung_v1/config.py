52ddb809bca7f9f4d2d19b9bf80d3cbc62ae66aa
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 16:48:53 +0800

    Update config.py

--- dlung_v1/config.py
+++ dlung_v1/config.py
@@ -1,6 +1,7 @@
 import os
 
-BASE = '/research/dept8/jzwang/code/lung_nodule_detector/dlung_v1/' # make sure you have the ending '/'
+# we need to change base only.Then put the data under BASE/data/sample
+BASE = '/research/dept8/jzwang/code/lung_nodule_detector/dlung_v1/' # make sure we have the ending '/'
 data_config = {
     "img_dir" : BASE + "data/",
     "data_txt" : BASE + "data/filedir.txt",
