d2f9e025c9db94b7d01725cd1ccb3ce225577491
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 18:42:50 +0800

    Update generate_list.py

--- dlung_v1/data/generate_list.py
+++ dlung_v1/data/generate_list.py
@@ -3,4 +3,6 @@ rootrecord = []
 dirrecord = []
 for root,dirs,files in os.walk("CT-Lung/"):
     if len(dirs) == 0:
-        print(root)
+        rootrecord.append(root)
+with open("filedir.txt",'w') as f:
+    f.write('\n'.join(rootrecord))
