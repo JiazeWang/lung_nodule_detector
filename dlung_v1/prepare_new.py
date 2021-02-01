b38021bd26fcef95f6e45ee00e1017a87aab9102
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 11:19:38 +0800

    Update prepare_new.py

--- dlung_v1/prepare_new.py
+++ dlung_v1/prepare_new.py
@@ -171,8 +171,8 @@ def savenpy_luna_attribute(params_lists):
     sliceim = apply_mask(sliceim, binary_mask1, binary_mask2)
     sliceim1, _ = resample(sliceim, spacing, resolution, order=1)
     sliceim = sliceim1[np.newaxis, ...]
-    np.save((savepath + '_clean.npy'), sliceim)
-    nrrd.write(savepath + '_clean.nrrd'), sliceim)
+    np.save(savepath + '_clean.npy', sliceim)
+    nrrd.write(savepath + '_clean.nrrd', sliceim)
     return 1
 
 def main():
