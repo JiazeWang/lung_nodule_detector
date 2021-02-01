ec9799b8f2aa69a8f67082ad4b6b0a6aa552e004
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 18:17:23 +0800

    Update generate_list.py

--- dlung_v1/data/generate_list.py
+++ dlung_v1/data/generate_list.py
@@ -1,19 +1,4 @@
-
 import os
-
-image_path = 'CT-Lung'
-def get_filelist(dir, Filelist):
-    newDir = dir
-    if os.path.isfile(dir):
-        Filelist.append(dir)
-    elif os.path.isdir(dir):
-        for s in os.listdir(dir):
-            newDir=os.path.join(dir,s)
-            get_filelist(newDir, Filelist)
-    return Filelist
-
-if __name__ =='__main__' :
-    list = get_filelist('CT-Lung', [])
-    print(len(list))
-    for e in list:
-        print(e)
+for root,dirs,files in os.walk(r"D:\test"):
+    for dir in dirs:
+        print(dir)
