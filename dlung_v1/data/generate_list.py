978097ed9a96e4113bbb4718142a33c215aa5b1c
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 18:31:49 +0800

    Update generate_list.py

--- dlung_v1/data/generate_list.py
+++ dlung_v1/data/generate_list.py
@@ -1,6 +1,10 @@
 import os
+root = []
+dirs = []
 for root,dirs,files in os.walk("CT-Lung/"):
+    root.append(root)
     for dir in dirs:
         pathnow = os.path.join(root, dir)
-        print("path:", pathnow)
-        print("files:", files)
+        dirs.append(dirs))
+print("root", root)
+print("dirs", dirs)
