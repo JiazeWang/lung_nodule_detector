ff217424159b6551280a932bafce5a77f68c6fe8
JiazeWang <jiazewang@csu.edu.cn>
2021-02-01 18:34:17 +0800

    Update generate_list.py

--- dlung_v1/data/generate_list.py
+++ dlung_v1/data/generate_list.py
@@ -2,9 +2,10 @@ import os
 rootrecord = []
 dirrecord = []
 for root,dirs,files in os.walk("CT-Lung/"):
-    rootrecord.append(root)
     for dir in dirs:
         pathnow = os.path.join(root, dir)
         dirrecord.append(dirs)
-print("root", rootrecord)
-print("dirs", dirrecord)
+        rootrecord.append(root)
+root = set(root)
+print("root", len(rootrecord))
+print("dirs", len(dirrecord))
