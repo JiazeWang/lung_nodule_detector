0a41c15864cabf60954dab9c8416bebf0de6d27a
jiazewang <115511436@link.cuhk.edu.hk>
2021-02-02 21:39:04 +0800

    Create config.py

---
+++ preprocess/config.py
@@ -0,0 +1,14 @@
+import os
+
+# we need to change base only.Then put the data under BASE/data/sample
+BASE = os.path.realpath(__file__)[:-9]  # assume the BASE dir is 'data'  ' # make sure we have the ending '/'
+data_config = {
+    "img_dir" : BASE + "data/",
+    "data_txt" : BASE + "data/filedir.txt",
+    "lung_mask_dir" : BASE + "data/lung_mask/",
+    "npy_dir" :  BASE +  "data/npy/",
+    "mhd_dir": BASE + "data/mhd/",
+    "result": BASE + "result/",
+    "record": "record_series_list.txt",
+}
+config = dict(data_config)
