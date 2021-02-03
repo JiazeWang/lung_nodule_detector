d4d2f393ab18a85d4aa3831a07eaaa861c172cbb
jiazewang <115114536@link.cuhk.edu.hk>
2021-02-03 22:32:30 +0800

    Update main.py

--- training/main.py
+++ training/main.py
@@ -169,7 +169,7 @@ def main():
     if args.test == 1:
         #margin = 32
         #sidelen = 144
-        margin = 0
+        margin = 24
         sidelen = 96
         print ("args.test True")
         split_comber = SplitComb(sidelen, config['max_stride'], config['stride'], margin, config['pad_value'])
@@ -221,7 +221,7 @@ def main():
 
     #margin = 32
     #sidelen = 144
-    margin = 0
+    margin = 24
     sidelen = 96
     split_comber = SplitComb(sidelen, config['max_stride'], config['stride'], margin, config['pad_value'])
     test_dataset = data.DataBowl3Detector(
