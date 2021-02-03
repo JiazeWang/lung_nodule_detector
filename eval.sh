667b566155130da5e70ec114fff63c2144d94086
jiazewang <115114536@link.cuhk.edu.hk>
2021-02-03 23:26:26 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,14 +1,14 @@
 sid="val9_sid.csv"
-submit="luna_submission_res18_split_classify.csv"
+submit="luna_submission_res18_split_30.csv"
 
 #cp ${sid} ./evaluationScript/annotations/
 #cp ${submit} ./evaluationScript/exampleFiles/submission/
 
 #cd evaluationScript
-mkdir ./evaluationScript_py3/exampleFiles/res18_split
+mkdir ./evaluationScript_py3/exampleFiles/res18_split_30
 python ./evaluationScript_py3/noduleCADEvaluationLUNA16.py \
         ./evaluationScript_py3/annotations/annotations.csv \
         ./evaluationScript_py3/annotations/annotations_excluded.csv \
         ${sid} \
         ${submit} \
-        ./evaluationScript_py3/exampleFiles/res18_split/
+        ./evaluationScript_py3/exampleFiles/res18_split_30/
