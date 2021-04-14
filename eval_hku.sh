d73b1d9c24eb8d0ed69e8f3afc7bb0d5e7e047f9
jiazewang <115114536@link.cuhk.edu.hk>
2021-04-14 23:38:54 +0800

    Update eval_hku.sh

--- eval_hku.sh
+++ eval_hku.sh
@@ -1,9 +1,9 @@
 sid="./preprocess/hku_list_test.csv"
-submit="/research/dept8/jzwang/code/lung_nodule_detector/hku_submission_5_new.csv"
+submit="/research/dept8/jzwang/code/lung_nodule_detector/hku_submission_20.csv"
 
 
 
-mkdir ./evaluationScript_py3/exampleFiles/hku_submission_5_new
+mkdir ./evaluationScript_py3/exampleFiles/hku_submission_20
 python ./evaluationScript_py3/noduleCADEvaluationLUNA16.py \
         ./preprocess/annotations_hku.csv \
         ./evaluationScript_py3/annotations/annotations_excluded.csv \
