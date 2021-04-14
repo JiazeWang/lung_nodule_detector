454741647c2e67791f71a0552ea1ab12f7a1f7a1
jiazewang <115114536@link.cuhk.edu.hk>
2021-04-14 15:36:49 +0800

    Revert "Revert "update_archor""
    This reverts commit 70176768f00cc54dbfaa4adb7d783d1e19e147bb.

---
+++ config_training_hku0.py
@@ -0,0 +1,9 @@
+config = {'luna_raw':'/root/ssd_data/LUNA/',
+          'luna_data':'/research/dept8/jzwang/dataset/LUNA16/combined/',
+          'preprocess_result_path':'/research/dept8/jzwang/dataset/HKU/preprocessed/numpy/',
+          'luna_abbr':'./labels/shorter.csv',
+          'luna_label':'./labels/lunaqualified_all.csv',
+          'luna_candidate_label':'./labels/luna_candidate_all.csv',
+          'lidc_xml':'./lidc_xml',
+          'preprocessing_backend':'python'
+         }
