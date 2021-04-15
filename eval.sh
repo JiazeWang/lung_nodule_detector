9262026b81aee5e90911488d90311bf89adcf85b
jiazewang <115114536@link.cuhk.edu.hk>
2021-04-15 13:56:50 +0800

    6912

--- eval.sh
+++ eval.sh
@@ -1,17 +1,17 @@
 sid="./preprocess/hku_list_test.csv"
-submit="'./hku_submission_5.csv'"
+submit="hku_submission_6912.csv'"
 
 #cp ${sid} ./evaluationScript/annotations/
 #cp ${submit} ./evaluationScript/exampleFiles/submission/
 
 #cd evaluationScript
-mkdir ./evaluationScript_py3/exampleFiles/res18_split_30
+mkdir ./evaluationScript_py3/exampleFiles/res18_split_6
 python ./evaluationScript_py3/noduleCADEvaluationLUNA16.py \
         ./evaluationScript_py3/annotations/annotations.csv \
         ./evaluationScript_py3/annotations/annotations_excluded.csv \
         ${sid} \
         ${submit} \
-        ./evaluationScript_py3/exampleFiles/res18_split_30/
+        ./evaluationScript_py3/exampleFiles/res18_split_6/
 
 mkdir ./evaluationScript_py3/exampleFiles/res18_split_30
 python ./evaluationScript_py3/noduleCADEvaluationLUNA16.py \
--- eval_hku.sh
+++ eval_hku.sh
@@ -1,12 +1,11 @@
 sid="./preprocess/hku_list_test.csv"
-submit="/research/dept8/jzwang/code/lung_nodule_detector/hku_submission_20.csv"
+submit="/research/dept8/jzwang/code/lung_nodule_detector/hku_submission_6912.csv"
 
 
-
-mkdir ./evaluationScript_py3/exampleFiles/hku_submission_20
+mkdir ./evaluationScript_py3/exampleFiles/hku_submission_6912
 python ./evaluationScript_py3/noduleCADEvaluationLUNA16.py \
         ./preprocess/annotations_hku.csv \
         ./evaluationScript_py3/annotations/annotations_excluded.csv \
         ${sid} \
         ${submit} \
-        ./evaluationScript_py3/exampleFiles/hku_submission_5_new/
+        ./evaluationScript_py3/exampleFiles/hku_submission_6912/
